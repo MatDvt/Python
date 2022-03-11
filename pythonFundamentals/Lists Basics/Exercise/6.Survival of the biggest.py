b7bce419e0e15c21fe20c7a70c01af851968bc23
Miro T <matcheto@gmail.com>
2022-03-11 16:31:13 +0200

    All

---
+++ pythonFundamentals/Lists Basics/Exercise/6.Survival of the biggest.py
@@ -0,0 +1,6 @@
+lst_ints = [int(x) for x in input().split()]
+n = int(input())
+
+for num in range(n):
+    lst_ints.remove(min(lst_ints))
+print(*lst_ints, sep=", ")
---
+++ pythonFundamentals/Lists Basics/Exercise/8. Seize the Fire.py
@@ -0,0 +1,33 @@
+all_cells = input().split("#")
+water = int(input())
+effort = 0
+fire = 0
+cells_values = []
+
+for cell in all_cells:
+    current_cell = cell.split(" = ")
+    type_of_fire = current_cell[0]
+    cell_value = int(current_cell[-1])
+
+    if type_of_fire == "High":
+        if not 81 <= cell_value <= 125:
+            continue
+    elif type_of_fire == "Medium":
+        if not 51 <= cell_value <= 80:
+            continue
+    elif type_of_fire == "Low":
+        if not 1 <= cell_value <= 50:
+            continue
+
+    if water < cell_value:
+        continue
+    cells_values.append(cell_value)
+    water -= cell_value
+    effort += cell_value * 0.25
+    fire += cell_value
+
+print("Cells:")
+for cell in cells_values:
+    print(f' - {cell}')
+print(f'Effort: {effort:.2f}')
+print(f'Total Fire: {fire}')
